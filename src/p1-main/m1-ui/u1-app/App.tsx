3eda79d7db00c2fa0b830c2659ce061a2c57b850
MihaThePooh <63107837+MihaThePooh@users.noreply.github.com>
2021-10-20 21:27:57 +0300

    second comit

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -15,6 +15,7 @@ class App extends Component {
                 {/*<HW3/>*/}
                 {/*<HW4/>*/}
                 <HW5/>
+                <span>1</span>
             </div>
         )
     }
